9b03ba843a60be6c041637debdbbc1bbe5b0ea09
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2024-02-15 10:15:49 +0100

    no message

---
+++ src/main/webapp/java.js
@@ -0,0 +1,37 @@
+/**
+ * caso 1
+ */
+var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+/**
+ * caso 2
+ */
+for (let i = 0; i<10; i++) {
+  
+
+document.write(i);
+}
+
+document.write(i);
+/**
+ * caso 3
+ */
+ var z = 5
+    let x = z+10, y = 12;
+   
+    document.write(x + y);
